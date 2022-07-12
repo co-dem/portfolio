5cde85afb51a890f722aaa8a51a9b3956c5fccf0
PaladinMan <96345940+Palad1nMan@users.noreply.github.com>
2022-07-12 16:29:31 +0300

    Add files via upload

---
+++ inline_menu_bot/config.py
@@ -0,0 +1,25 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+TOKEN = '5317213631:AAHDUv3QjY6Y4aU_PIsCWe-fzIFcZjWS-e8'
+
+page_1 = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [InlineKeyboardButton(text = 'me', callback_data = 'myself')], 
+        [InlineKeyboardMarkup(text = '➡️', callback_data = 'next_to_page_2')] 
+    ], 
+    row_width = 1
+)
+
+page_2 = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [InlineKeyboardButton(text = 'experience', callback_data = 'exp'), InlineKeyboardButton(text = 'skills', callback_data = 'skil')],
+        [InlineKeyboardButton(text = '⬅️', callback_data = 'back_to_page_1'), InlineKeyboardButton(text = '➡️', callback_data = 'next_to_page_3')]
+    ]
+)
+
+page_3 = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [InlineKeyboardButton(text = 'price', callback_data = 'prices')],
+        [InlineKeyboardButton(text = '⬅️', callback_data = 'back_to_page_2')]
+    ]
+)
---
+++ inline_menu_bot/menu_main.py
@@ -0,0 +1,38 @@
+from aiogram import *
+from aiogram.utils import executor
+
+from config import TOKEN, page_1, page_2, page_3
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands = 'start')
+async def welcome(message: types.Message):
+    await bot.send_message(message.from_user.id, "Hello, I'm a menu bot by codem\nI was made for codem's portfolio\nenjoy!", reply_markup = page_1)
+
+@dp.callback_query_handler(text = ['myself', 'exp', 'skil', 'prices', 'back_to_page_1', 'back_to_page_2', 'next_to_page_2', 'next_to_page_3'])
+async def main(call: types.CallbackQuery):
+    #*---------- main questions ----------*#
+    if call.data == 'myself':
+        await bot.send_message(call.from_user.id, 'some info about me')
+    elif call.data == 'exp':
+        await bot.send_message(call.from_user.id, 'some info about experience I got')
+    elif call.data == 'skil':
+        await bot.send_message(call.from_user.id, 'some info about my skills')
+    elif call.data == 'prices':
+        await bot.send_message(call.from_user.id, 'some info about my prices')
+    
+    #*---------- page switching ----------*#
+    elif call.data == 'back_to_page_1':
+        await bot.delete_message(chat_id = call.from_user.id, message_id = call.message.message_id)
+        await bot.send_message(call.from_user.id, 'here you can get some info about me', reply_markup = page_1)
+
+    elif call.data == 'back_to_page_2' or call.data == 'next_to_page_2':
+        await bot.delete_message(chat_id = call.from_user.id, message_id = call.message.message_id)
+        await bot.send_message(call.from_user.id, 'this is page number 2 with some useful info', reply_markup = page_2)
+
+    elif call.data == 'next_to_page_3':
+        await bot.delete_message(chat_id = call.from_user.id, message_id = call.message.message_id)
+        await bot.send_message(call.from_user.id, 'this is page number 3 with some price info', reply_markup = page_3)
+
+executor.start_polling(dp)
